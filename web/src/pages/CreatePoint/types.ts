86bf65f195e017426c7a2c7d3da38f71e04a3846
alanzdr <alandersonzelindrodr@gmail.com>
2020-06-03 20:06:28 -0300

    03/06/2020

---
+++ web/src/pages/CreatePoint/types.ts
@@ -0,0 +1,13 @@
+export interface Item {
+  id: number,
+  title: string,
+  image_url: string
+}
+
+export interface IBGE_UF {
+  sigla: string
+}
+
+export interface IBGE_CITY {
+  nome: string
+}
